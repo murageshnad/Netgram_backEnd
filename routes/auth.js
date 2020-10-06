a5d0feb9e10ba69b14ac9a3fddaccb973e9d850a
muragesh <muragesh@netprise.co.in>
2020-10-06 11:59:40 +0530

    Added routes

---
+++ routes/auth.js
@@ -0,0 +1,106 @@
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose')
+const User = mongoose.model("User")
+const crypto = require('crypto')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const requireLogin = require('../middleware/requireLogin');
+const secret = 'some other secret as default';
+//
+
+
+
+
+router.post('/signup', (req, res) => {
+    const { name, email, password, pic } = req.body
+    if (!email || !password || !name) {
+        return res.status(422).json({ error: "please add all the fields" })
+    }
+    User.findOne({ email: email })
+        .then((savedUser) => {
+            if (savedUser) {
+                return res.status(422).json({ error: "user already exists with that email" })
+            }
+            bcrypt.hash(password, 12)
+                .then(hashedpassword => {
+                    const user = new User({
+                        email,
+                        password: hashedpassword,
+                        name,
+                        pic
+                    });
+
+                    user.save()
+                        .then(user => {
+
+                            res.json({ message: "saved successfully" })
+                        })
+                        .catch(err => {
+                            console.log(err)
+                        })
+                })
+
+        })
+        .catch(err => {
+            console.log(err)
+        })
+})
+
+
+router.post('/signin', (req, res) => {
+    const { email, password } = req.body
+    if (!email || !password) {
+        return res.status(422).json({ error: "please add email or password" })
+    }
+    User.findOne({ email: email })
+        .then(savedUser => {
+            if (!savedUser) {
+                return res.status(422).json({ error: "Invalid Email or password" })
+            }
+            bcrypt.compare(password, savedUser.password)
+                .then(doMatch => {
+                    if (doMatch) {
+                        // res.json({message:"successfully signed in"})
+                        const token = jwt.sign({ _id: savedUser._id }, secret)
+                        const { _id, name, email, followers, following, pic } = savedUser
+                        res.json({ token, user: { _id, name, email, followers, following, pic } })
+                    }
+                    else {
+                        return res.status(422).json({ error: "Invalid Email or password" })
+                    }
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        })
+})
+
+
+
+
+
+router.post('/new-password', (req, res) => {
+    const newPassword = req.body.password
+    const sentToken = req.body.token
+    User.findOne({ resetToken: sentToken, expireToken: { $gt: Date.now() } })
+        .then(user => {
+            if (!user) {
+                return res.status(422).json({ error: "Try again session expired" })
+            }
+            bcrypt.hash(newPassword, 12).then(hashedpassword => {
+                user.password = hashedpassword
+                user.resetToken = undefined
+                user.expireToken = undefined
+                user.save().then((saveduser) => {
+                    res.json({ message: "password updated success" })
+                })
+            })
+        }).catch(err => {
+            console.log(err)
+        })
+})
+
+
+module.exports = router
